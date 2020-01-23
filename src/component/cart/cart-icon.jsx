21a6ac720e52bac064af9fb0bdb39f9f3b166de9
Sangita awaghad <sangita.awaghad@gmail.com>
2020-01-23 17:12:35 +0530

    Added:functionality to update the total price on incrementing or decrimenting purchase book count

--- src/component/cart/cart-icon.jsx
+++ src/component/cart/cart-icon.jsx
@@ -6,49 +6,53 @@ class CartIcon extends Component {
     constructor(props) {
         super(props)
         this.count = 1
-        this.totalPrice = 0
+        
         this.state = {
             item: null,
-
+            purchaseBookIndividualCount: [],
+            totalPrice: 0
         }
 
         this.state.item = this.props.location.state.selectedBooks
-        console.log("====>", this.state.item[0].image)
-        // console.log("===>"+this.state.item.image)
+        this.state.item.map((item)=>{
+            { this.state.totalPrice = this.state.totalPrice + parseInt(item.price) }
+        })
     }
 
-    decrement = () => {
-        if (this.count > 1)
-            this.count--
-        this.setState({ count: this.count })
+    decrement = (i, price) => {
+        if (this.state.purchaseBookIndividualCount[i] > 1) {
+            this.state.purchaseBookIndividualCount[i]--;
+            this.setState({ purchaseBookIndividualCount: this.state.purchaseBookIndividualCount });
+            this.state.totalPrice=this.state.totalPrice-parseInt(price);
+            this.setState({totalPrice:this.state.totalPrice})
+        }
     }
 
-    increment = () => {
-        this.count++
-        this.setState({ count: this.count })
+    increment = (i,price) => {
+        this.state.purchaseBookIndividualCount[i]++;
+        this.setState({ purchaseBookIndividualCount: this.state.purchaseBookIndividualCount });
+        this.state.totalPrice=this.state.totalPrice+parseInt(price);
+        this.setState({totalPrice:this.state.totalPrice})
     }
 
     remove = (item) => {
         console.log(this.state.item.length);
         let a;
-        for(let i=0;i<this.state.item.length;i++) {
+        for (let i = 0; i < this.state.item.length; i++) {
             // a = this.state.item[i].find(o => o.title == item.title)
-            if(this.state.item[i].title==item.title)
-            {console.log("ssss")
-            this.state.item.splice(i,1);
-            this.setState({item:this.state.item})
+            if (this.state.item[i].title == item.title) {
+                console.log("ssss")
+                this.state.item.splice(i, 1);
+                this.setState({ item: this.state.item })
             }
         }
-        
-        console.log("===>"+a);
-        
-        console.log(item.title);
     }
 
     render() {
         var Books = this.state.item.map((item, i) => {
-            console.log(item)
-            { this.totalPrice = this.totalPrice + parseInt(item.price) }
+            this.state.purchaseBookIndividualCount.push(1)
+            // { this.state.totalPrice = this.state.totalPrice + parseInt(item.price) }
+            
             return (
                 <div>
                     <div className="cart-image1">
@@ -57,9 +61,9 @@ class CartIcon extends Component {
                             <div className="book-author">{item.author}</div>
                             <div className="book-price" > Rs.{item.price}</div>
                             <div>
-                                <button className="minus-plus" onClick={() => { this.decrement() }}>-</button>
-                                <input className="text" value={this.count} ></input>
-                                <button className="minus-plus" onClick={() => { this.increment() }}>+</button>
+                                <button className="minus-plus" onClick={() => { this.decrement(i,item.price) }}>-</button>
+                                <input className="text" value={this.state.purchaseBookIndividualCount[i]} ></input>
+                                <button className="minus-plus" onClick={() => { this.increment(i,item.price) }}>+</button>
                                 <button className="remove" onClick={() => { this.remove(item) }}>Remove</button>
                             </div>
                         </div>
@@ -72,7 +76,7 @@ class CartIcon extends Component {
                 <div className="border">
                     <div className="cart-title">My Cart({this.state.item.length})</div>
                     {Books}
-                    <div className="total-price"><p id="totalprice">Total Price: {this.totalPrice}</p></div>
+                    <div className="total-price"><p id="totalprice">Total Price: {this.state.totalPrice}</p></div>
                     <button className="place-order">PLACE ORDER</button>
                 </div>
                 {/* <Customer/> */}
