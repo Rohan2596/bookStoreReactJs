ef6c698fd6f6b695f274bffe2a876e08bba0031f
Sangita awaghad <sangita.awaghad@gmail.com>
2020-01-23 16:04:30 +0530

    Refactored:make code hygine

--- src/component/allBooks/allBooks.jsx
+++ src/component/allBooks/allBooks.jsx
@@ -20,7 +20,7 @@ class AllBooks extends Component {
       this.props.action(item);
 
    }
-
+   
 
    render() {
       var Books = this.props.getAllBooksData.map((item, i) => {
@@ -28,20 +28,20 @@ class AllBooks extends Component {
          return (
             <div className="div" >
                <Card className='card'>
-                  <div className='info' id={i}>
-                     <div className='imageSpace' >
-                        <img className='bookImg' src={item.image} alt={"bookImg"} ></img>
-                     </div>
-                     <div className='bookName'>{item.title}</div>
-                     <div className='authorName'>{item.author}</div>
-                     <div className='bookName'>Rs.{item.price}</div>
-                     <Card className="card bookInfo" >
-                        <p id="bookTitleForDescription" gutterBottom> Book Detail</p>
-                        <p id="description">{item.description}</p>
-                     </Card>
+               <div className='info' id={i}>
+                  <div className='imageSpace' >
+                     <img className='bookImg' src={item.image} alt={"bookImg"} ></img>
                   </div>
-                  <button className='beforeAddToBag' hidden={this.state.addToBagCss[i]} onClick={() => { this.addToCart(item, i) }} >ADD TO BAG</button>
-                  <button className='afterAddToBag' hidden={!this.state.addToBagCss[i]} disabled >ADDED TO BAG</button>
+                  <div className='bookName'>{item.title}</div>
+                  <div className='authorName'>{item.author}</div>
+                  <div className='bookName'>Rs.{item.price}</div>
+                  <Card className="card bookInfo" >
+                     <p id="bookTitleForDescription" gutterBottom> Book Detail</p>
+                     <p id="description">{item.description}</p>
+                  </Card>
+               </div>
+               <button className='beforeAddToBag' hidden={this.state.addToBagCss[i]} onClick={() => { this.addToCart(item, i) }} >ADD TO BAG</button>
+               <button className='afterAddToBag' hidden={!this.state.addToBagCss[i]} disabled >ADDED TO BAG</button>
                </Card>
             </div>
          )
