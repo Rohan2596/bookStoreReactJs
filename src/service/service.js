de1716b5e2ba6cbe49157aa7fa1c2a44e637610a
suraj <surajpj7852@gmail.com>
2020-01-23 18:38:29 +0530

    Refactored url

--- src/service/service.js
+++ src/service/service.js
@@ -2,10 +2,13 @@ import Axios from "axios"
 
 // import  {postAxios,getAxios} from ''
 export default class Service {
+    constructor(){
+        this.Url = "http://18.189.22.123:3000/";
+    }
     getAllBooks() {
         return Axios({
             method: 'GET',
-            url: 'http://3.19.232.27:3000/getBookDetails'
+            url: this.Url+'getBookDetails'
         })
             .then(result => {
                 console.log(result);
@@ -15,7 +18,7 @@ export default class Service {
     searchBookByTitle(serchData){
         return Axios({
             method: 'GET',
-            url: 'http://3.19.232.27:3000/searchBookByTitle?title='+serchData
+            url: this.Url+'searchBookByTitle?title='+serchData
         })
             .then(result => {
                 return result;
@@ -24,7 +27,7 @@ export default class Service {
     customerDetails(details){
         return Axios({
             method: 'POST',
-            url: 'http://3.19.232.27:3000/customerDetails',
+            url: this.Url+'customerDetails',
             data: details
         })
             .then(result => {
@@ -34,7 +37,7 @@ export default class Service {
     sortBook(data){
         return Axios({
             method: 'GET',
-            url: 'http://3.19.232.27:3000/sortBookByAttribute?attribute='+data
+            url: this.Url+'sortBookByAttribute?attribute='+data
         })
             .then(result => {
                 return result;
