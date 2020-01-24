780f708c4138da94e57bd0df45c2b737b5220657
Sangita awaghad <sangita.awaghad@gmail.com>
2020-01-24 16:32:39 +0530

    Added:ui for pagination

--- src/component/allBooks/allBooks.jsx
+++ src/component/allBooks/allBooks.jsx
@@ -2,52 +2,61 @@ import React, { Component } from 'react';
 import './allBooks.css';
 import { withRouter } from 'react-router-dom';
 import Card from '@material-ui/core/Card'
-
+import JwPagination from 'jw-react-pagination';
 
 class AllBooks extends Component {
    constructor(props) {
       super(props);
       this.array = [];
+      var exampleItems = this.props.getAllBooksData;
       this.state = {
          item: null,
-         addToBagCss: []
+         addToBagCss: [],
+         exampleItems: exampleItems,
+         pageOfItems: []
       }
+      this.onChangePage = this.onChangePage.bind(this);
+     
    };
+   onChangePage(pageOfItems) {
+      this.setState({ pageOfItems: pageOfItems });
+   }
    addToCart = (item, i) => {
       this.state.addToBagCss[i] = true;
       this.setState({ addToBagCss: this.state.addToBagCss })
       this.props.action(item);
 
    }
-   
-
    render() {
-      var Books = this.props.getAllBooksData.map((item, i) => {
+      this.state.exampleItems = this.props.getAllBooksData;
+      var books = this.state.pageOfItems.map((item, i) => {
          this.state.addToBagCss.push(false);
          return (
-            <div className="div" >
+            <div className="div" id={i + 1}>
                <Card className='card'>
-               <div className='info' id={i}>
-                  <div className='imageSpace' >
-                     <img className='bookImg' src={item.image} alt={"bookImg"} ></img>
+                  <div className='info' id={i}>
+                     <div className='imageSpace' >
+                        <img className='bookImg' src={item.image} alt={"bookImg"} ></img>
+                     </div>
+                     <div className='bookName'>{item.title}</div>
+                     <div className='authorName'>{item.author}</div>
+                     <div className='bookName'>Rs.{item.price}</div>
+                     <Card className="card bookInfo" >
+                        <p id="bookTitleForDescription" gutterBottom> Book Detail</p>
+                        <p id="description">{item.description}</p>
+                     </Card>
                   </div>
-                  <div className='bookName'>{item.title}</div>
-                  <div className='authorName'>{item.author}</div>
-                  <div className='bookName'>Rs.{item.price}</div>
-                  <Card className="card bookInfo" >
-                     <p id="bookTitleForDescription" gutterBottom> Book Detail</p>
-                     <p id="description">{item.description}</p>
-                  </Card>
-               </div>
-               <button className='beforeAddToBag' hidden={this.state.addToBagCss[i]} onClick={() => { this.addToCart(item, i) }} >ADD TO BAG</button>
-               <button className='afterAddToBag' hidden={!this.state.addToBagCss[i]} disabled >ADDED TO BAG</button>
+                  <button className='beforeAddToBag' hidden={this.state.addToBagCss[i]} onClick={() => { this.addToCart(item, i) }} >ADD TO BAG</button>
+                  <button className='afterAddToBag' hidden={!this.state.addToBagCss[i]} disabled >ADDED TO BAG</button>
                </Card>
             </div>
          )
+
       })
       return (
          <div>
-            {Books}
+            {books}
+            <div><JwPagination items={this.state.exampleItems} onChangePage={this.onChangePage} /></div>
          </div>
       )
    }
--- src/component/cart/cart-icon.jsx
+++ src/component/cart/cart-icon.jsx
@@ -6,7 +6,7 @@ class CartIcon extends Component {
     constructor(props) {
         super(props)
         this.count = 1
-        
+
         this.state = {
             item: null,
             purchaseBookIndividualCount: [],
@@ -14,63 +14,60 @@ class CartIcon extends Component {
         }
 
         this.state.item = this.props.location.state.selectedBooks
-        this.state.item.map((item)=>{
+        this.state.item.map((item) => {
             { this.state.totalPrice = this.state.totalPrice + parseInt(item.price) }
         })
     }
 
     decrement = (i, price) => {
-        if (this.state.purchaseBookIndividualCount[i] > 1) {
-            this.state.purchaseBookIndividualCount[i]--;
-            this.setState({ purchaseBookIndividualCount: this.state.purchaseBookIndividualCount });
-            this.state.totalPrice=this.state.totalPrice-parseInt(price);
-            this.setState({totalPrice:this.state.totalPrice})
-        }
-    }
-
-    increment = (i,price) => {
-        this.state.purchaseBookIndividualCount[i]++;
-        this.setState({ purchaseBookIndividualCount: this.state.purchaseBookIndividualCount });
-        this.state.totalPrice=this.state.totalPrice+parseInt(price);
-        this.setState({totalPrice:this.state.totalPrice})
-    }
-
-    remove = (item) => {
-        console.log(this.state.item.length);
-        let a;
-        for (let i = 0; i < this.state.item.length; i++) {
-            // a = this.state.item[i].find(o => o.title == item.title)
-            if (this.state.item[i].title == item.title) {
-                this.state.totalPrice=this.state.totalPrice-parseInt(item.price);
-                this.setState({totalPrice:this.state.totalPrice})
-                this.state.item.splice(i, 1);
-                this.setState({ item: this.state.item })
+        if (this.state.purchaseBookIndividualCount[i] != undefined) {
+            if (this.state.purchaseBookIndividualCount[i] > 1) {
+                this.state.purchaseBookIndividualCount[i]--;
+                this.setState({ purchaseBookIndividualCount: this.state.purchaseBookIndividualCount });
+                this.state.totalPrice = this.state.totalPrice - parseInt(price);
+                this.setState({ totalPrice: this.state.totalPrice })
             }
         }
+    }
+
+    increment = (i, price) => {
+        if (this.state.purchaseBookIndividualCount[i] != undefined) {
+            this.state.purchaseBookIndividualCount[i]++;
+            this.setState({ purchaseBookIndividualCount: this.state.purchaseBookIndividualCount });
+            this.state.totalPrice = this.state.totalPrice + parseInt(price);
+            this.setState({ totalPrice: this.state.totalPrice })
+        }
+    }
+
+    remove = (i, price) => {
+        this.state.totalPrice = this.state.totalPrice - (parseInt(price)*this.state.purchaseBookIndividualCount[i]);
+        this.setState({ totalPrice: this.state.totalPrice })
+        this.state.item.splice(i, 1, undefined);
+        this.setState({ item: this.state.item })
     }
 
     render() {
         var Books = this.state.item.map((item, i) => {
             this.state.purchaseBookIndividualCount.push(1)
-            // { this.state.totalPrice = this.state.totalPrice + parseInt(item.price) }
-            
-            return (
-                <div>
-                    <div className="cart-image1">
-                        <img className="image" src={item.image}></img>
-                        <div className="book-title">{item.title}
-                            <div className="book-author">{item.author}</div>
-                            <div className="book-price" > Rs.{item.price}</div>
-                            <div>
-                                <button className="minus-plus" onClick={() => { this.decrement(i,item.price) }}>-</button>
-                                <input className="text" value={this.state.purchaseBookIndividualCount[i]} ></input>
-                                <button className="minus-plus" onClick={() => { this.increment(i,item.price) }}>+</button>
-                                <button className="remove" onClick={() => { this.remove(item) }}>Remove</button>
+            if (item != undefined) {
+                return (
+                    <div>
+                        <div className="cart-image1">
+                            <img className="image" src={item.image}></img>
+                            <div className="book-title">{item.title}
+                                <div className="book-author">{item.author}</div>
+                                <div className="book-price" > Rs.{item.price}</div>
+                                <div>
+                                    <button className="minus-plus" onClick={() => { this.decrement(i, item.price) }}>-</button>
+                                    <input className="text" value={this.state.purchaseBookIndividualCount[i]} ></input>
+                                    <button className="minus-plus" onClick={() => { this.increment(i, item.price) }}>+</button>
+                                    <button className="remove" onClick={() => { this.remove(i,item.price) }}>Remove</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            )
+                )
+            }
         })
         return (
             <div>
